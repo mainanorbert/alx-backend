79b7c5faf9652ee7f2ed97fe0f00a1f2bea5c6f9
Norberts <mainanorbert@gmail.comm>
2024-03-31 20:19:10 +0300

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" module for Simple helper function"""
+
+
+def index_range(page, page_size):
+    """function that implements simple helper function 
+    for pagination"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
