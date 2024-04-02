b574150c2f2b278db6a74db640806de29b36c805
Norberts <mainanorbert@gmail.comm>
2024-04-02 19:41:25 +0300

    least recently used caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -9,10 +9,12 @@ class LRUCache(BaseCaching):
     from BaseCaching and is a
     caching system"""
     def __init__(self):
+        """initializing class"""
         super().__init__()
         self.order = []
 
     def put(self, key, item):
+        """caching data"""
         if key and item:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 lru_k = self.order.pop(0)
@@ -22,6 +24,7 @@ class LRUCache(BaseCaching):
             self.order.append(key)
 
     def get(self, key):
+        """getting cached data"""
         if key is None or key not in self.cache_data.keys():
             return None
         self.order.remove(key)
