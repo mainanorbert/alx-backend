e9a02368083a758f4adb45a8911c7014f5f7977e
Norberts <mainanorbert@gmail.comm>
2024-04-02 19:31:21 +0300

    simple cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,13 +9,16 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """ Class Create a class BasicCache"""
     def __init__(self):
+        """initializing a class"""
         super().__init__()
 
     def put(self, key, item):
+        """putting to cache"""
         if key is not None or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """getting cached data"""
         if not key or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
