c575397ab4ba167fbd65aa851adf511172d74c1f
Norberts <mainanorbert@gmail.comm>
2024-04-02 19:39:34 +0300

    LIFO caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -9,9 +9,11 @@ class LIFOCache(BaseCaching):
     inherits from BaseCaching and is
     a caching system"""
     def __init__(self):
+        """initializing data"""
         super().__init__()
 
     def put(self, key, item):
+        """caching data"""
         if key and item:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 my_key, my_value = self.cache_data.popitem()
@@ -19,6 +21,7 @@ class LIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
+        """getting cached data"""
         if not key or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
