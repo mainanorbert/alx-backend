e7d78db102cf66dd8a24f327b04858e04774dd70
Norberts <mainanorbert@gmail.comm>
2024-04-02 17:41:51 +0300

    FIFO

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -20,5 +20,6 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        if key and key in self.cache_data.keys():
-            return self.cache_data[key]
+        if not key or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
