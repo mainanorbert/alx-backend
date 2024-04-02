717f4f5c951e3f0f3a946a619d5f5d6208f8a404
Norberts <mainanorbert@gmail.comm>
2024-04-02 19:37:58 +0300

    FIFO

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -9,9 +9,11 @@ class FIFOCache(BaseCaching):
     from BaseCaching and is a
     caching system"""
     def __init__(self):
+        """initialising class"""
         super().__init__()
 
     def put(self, key, item):
+        """putting into cache"""
         if key and item:
             if len(self.cache_data) >= self.MAX_ITEMS:
                 my_key = next(iter(self.cache_data))
@@ -20,6 +22,7 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
+        """getting cached data"""
         if not key or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
