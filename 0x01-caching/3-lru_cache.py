a83daede7fa2449f1cd11788c641f2f2ce44f645
Norberts <mainanorbert@gmail.comm>
2024-04-02 17:27:14 +0300

    3. LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""module for LRU Caching"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """class LRUCache that inherits
+    from BaseCaching and is a
+    caching system"""
+    def __init__(self):
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                lru_k = self.order.pop(0)
+                del self.cache_data[lru_k]
+                print(f"DISCARD: {lru_k}")
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        if key is None or key not in self.cache_data.keys():
+            return None
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data[key]
