847fc5ee9da431a726d02f4c5639adc73b60b6df
Norberts <mainanorbert@gmail.comm>
2024-04-02 20:16:09 +0300

    most recently used

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -9,19 +9,22 @@ class MRUCache(BaseCaching):
     from BaseCaching and is a
     caching system"""
     def __init__(self):
+        """initializing data"""
         super().__init__()
         self.order = []
 
     def put(self, key, item):
+        """caching data"""
         if key and item:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                lru_k = self.order[-1]
-                del self.cache_data[lru_k]
-                print(f"DISCARD: {lru_k}")
+                mru_k = self.order[-1]
+                del self.cache_data[mru_k]
+                print(f"DISCARD: {mru_k}")
             self.cache_data[key] = item
             self.order.append(key)
 
     def get(self, key):
+        """getting  cached data"""
         if key is None or key not in self.cache_data.keys():
             return None
         self.order.remove(key)
