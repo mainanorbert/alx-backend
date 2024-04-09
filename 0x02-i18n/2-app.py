e1993ed3ec396c74a901d9adb44dd1eca7be2dfa
Norberts <mainanorbert@gmail.comm>
2024-04-09 12:55:19 +0300

    Get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Get locale from request"""
+
+from flask import Flask, render_template, request
+from typing import Any
+from flask_babel import Babel
+from werkzeug.utils import cached_property
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """class with local"""
+    LANGUAGES = ['en', 'fr']
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Any:
+    """getting local"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> Any:
+    """Get locale from request"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
