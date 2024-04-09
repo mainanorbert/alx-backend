6523c3ad3d7201a20d149b126f905d8a7fbfd183
Norberts <mainanorbert@gmail.comm>
2024-04-09 09:35:17 +0300

    flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from typing import Any
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def index() -> Any:
+    """rendering a template"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
