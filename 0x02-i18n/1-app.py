9f4d606e5e542cd41a7cafda7b8c2435fc01f6ad
Norberts <mainanorbert@gmail.comm>
2024-04-09 10:48:55 +0300

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+from typing import Any
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    
+app = Flask('__name__')
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> Any:
+    '''defining locale'''
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
