f763e8c118d5be22fb785570f11db39a1968b947
Norberts <mainanorbert@gmail.comm>
2024-04-09 12:41:41 +0300

    babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -9,6 +9,8 @@ from typing import Any
 class Config:
     '''class with locale'''
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask('__name__')
