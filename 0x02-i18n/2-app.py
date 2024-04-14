dc751a92613dcd2de83e8f1f42a88067e55955ec
Norberts <mainanorbert@gmail.comm>
2024-04-14 21:11:50 +0300

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,7 +25,9 @@ def get_locale() -> str:
     """getting local"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/')
 def index() -> str:
