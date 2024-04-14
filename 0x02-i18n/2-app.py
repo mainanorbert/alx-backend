8472ddf9349496cbc4e7175baa501424bad8466b
Norberts <mainanorbert@gmail.comm>
2024-04-14 21:00:51 +0300

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localeselector
+@babel\.localeselector
 def get_locale() -> str:
     """getting local"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
