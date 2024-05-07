5180445dd3838801ff358823be27ad383d912217
Norberts <mainanorbert@gmail.comm>
2024-05-07 09:23:26 +0300

    Node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -7,7 +7,7 @@ client.on('connect', ()=>{
     console.log('Redis client connected to the server');
 });
 client.on('error', (error)=>{
-    console.log(`Redis client not connected to the server: ${error.message}`);
+    console.error(`Redis client not connected to the server: ${error}`);
 });
 
 const setNewSchool= (schoolName, value) =>{
